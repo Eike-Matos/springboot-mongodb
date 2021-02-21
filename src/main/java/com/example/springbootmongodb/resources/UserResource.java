1b85819bb58409bfe773617802e40ceecb909fa9
Eike Matos <eikeadrian9830@gmail.com>
2021-02-20 21:16:30 -0300

    User by Id

--- src/main/java/com/example/springbootmongodb/resources/UserResource.java
+++ src/main/java/com/example/springbootmongodb/resources/UserResource.java
@@ -1,4 +1,4 @@
-package com.example.springbootmongodb.resource;
+package com.example.springbootmongodb.resources;
 
 
 import java.util.List;
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -27,6 +28,13 @@ public class UserResource {
 		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList()); // Método utilizando expressão lambda para converter list em listDTO //
 		return ResponseEntity.ok().body(listDto);
 	}
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(new UserDTO(obj));
 
+	}
+		
 
 }
